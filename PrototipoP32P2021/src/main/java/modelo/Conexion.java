52eaf67871f4d6494a4b54f51bd7558bbafde12c
TagoSMD <jacobgt890@gmail.com>
2021-04-23 11:14:38 -0600

    Se añadieron paquetes para controlador , modelo y vista  y se añadio login

---
+++ PrototipoP32P2021/src/main/java/modelo/Conexion.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo;
+
+import java.sql.*;
+
+/**
+ * 
+ * @author Santiago Martinez Diaz
+ */
+public class Conexion {
+    
+        private static final String JDBC_URL = "jdbc:mysql://localhost/didactica2?useSSL=false&serverTimezone=UTC";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASS = "1234";
+    
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASS);
+    }
+    
+    public static void close(ResultSet rs){
+        try {
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(PreparedStatement stmt){
+        try {
+            stmt.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(Connection conn){
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+           ex.printStackTrace(System.out);
+        }
+    }
+    
+
+
+}
